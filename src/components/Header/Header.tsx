5ab52aa218bf2c4a1d21f4fe41f4f65de451dc89
n <n@ns-Mac-mini.local>
2025-12-15 00:13:57 +0100

    feat: burguermenu scaffolding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux";
+import { selectUserSession } from "../../features/users/store/selectors/userSelectors";
+import BurguerMenu from "../BurguerMenu/BurguerMenu";
+
 const Header = () => {
+    const session = useSelector(selectUserSession);
+    
     return (<>
         <header className="flex items-center justify-between px-4 py-4 fixed top-0 left-0 w-full">
-            <button className="text-secondary text-2xl">☰</button>
+            { session ? <BurguerMenu /> : <></> }
             <a href="/">
                 <h1 className="text-secondary font-bold text-lg">OMDB Films</h1>
             </a>
