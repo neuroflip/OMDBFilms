1b0aabab22e6c06ed003bddf3e7b06d6dc7c879a
neuroflip <jaanguita@gmail.com>
2025-12-22 18:04:47 +0100

    info: added components screenshot in README, fix: added useSession in Loading and register pages to autologin if needed

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,8 +1,11 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import RegisterForm from "../../features/users/ResgisterForm/RegisterForm";
+import useSession from "../../hooks/useSession";
 
 const Register = () => {
+  useSession("/search");
+
   return (<>
     <Header />
     <main className="flex flex-col w-full min-h-[80vh]">
